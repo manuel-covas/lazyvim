3c8346606fa35f4fc2bcf51b13629d66d899753b
Manuel Covas <manuel.p.covas@gmail.com>
2024-11-04 16:13:11 +0000

    [pyright] Set analysis diagnosticMode to "workspace"

---
+++ lua/plugins/pyright.lua
@@ -0,0 +1,23 @@
+require("lspconfig")
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                diagnosticMode = "workspace"
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
